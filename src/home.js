26ef4e83a7fadd133a56ac0b268fd812fe5ea025
bradlloy95 <136913328+bradlloy95@users.noreply.github.com>
2024-07-03 21:28:09 +0100

    updated basic content so nav is created in index.js

--- src/home.js
+++ src/home.js
@@ -1,7 +1,20 @@
+
+
 export function loadHome() {
-    const content = document.getElementById('content');
     
-    const homeDiv = document.createElement('div');
-    homeDiv.textContent = "Welcome to the Restaurant";
+    const content = document.getElementById('content');   
+    //add some text 
+    const homeDiv = document.createElement('div');   
     content.appendChild(homeDiv);
+
+    homeDiv.classList.add('homeDiv')
+    // add title to page
+    const homeTitle = document.createElement('h1');
+    homeTitle.textContent = 'Home'
+    const homeText = document.createElement('p');
+    homeText.textContent = 'Welcome to my restaurant';
+    homeDiv.appendChild(homeTitle)
+    homeDiv.appendChild(homeText)
+    const homeBTN = document.getElementById('homeBTN');
+    homeBTN.classList.add('clickedBTN')
 }
--- src/index.js
+++ src/index.js
@@ -2,35 +2,55 @@ import { loadHome } from "./home";
 import { loadMenu } from "./menu";
 import { loadAbout } from "./about";
 const content = document.getElementById('content');
+const header = document.getElementById('header')  
+
 
 function init() {
     clearContent()
+
     loadHome()
 }
 
 function clearContent(){
+    header.innerHTML = '';
+    createNav()
     content.innerHTML = '';
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    const homeBTN = document.getElementById('homeBTN');
-    const menuBTN = document.getElementById('menuBTN');
-    const aboutBTN = document.getElementById('aboutBTN');
+function createNav() {
 
+    const nav = document.createElement('nav')    
+
+    const homeBTN = document.createElement('button');
+    homeBTN.textContent = 'Home'
+    homeBTN.setAttribute('id', 'homeBTN')
     homeBTN.addEventListener('click', () => {
         clearContent()
         loadHome()
     })
+
+    const menuBTN = document.createElement('button');
+    menuBTN.textContent = 'Menu';
     menuBTN.addEventListener('click', () => {
         clearContent()
         loadMenu()
     })
+
+    const aboutBTN = document.createElement('button');
+    aboutBTN.textContent = 'About';
     aboutBTN.addEventListener('click', () => {
         clearContent()
         loadAbout()
     })
+
+    nav.appendChild(homeBTN)
+    nav.appendChild(menuBTN)
+    nav.appendChild(aboutBTN)
+    header.appendChild(nav);    
     
+}
 
-
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('loaded')
+    init()
 })
-init()
